0a6187baeb3b7f3ca0c29957a9beaf5ff56a67e7
Song <791589545@qq.com>
2026-01-28 11:49:59 +0800

    修改数据错误模型类

--- qs_event_analytics/lib/analytic_error_model.dart
+++ qs_event_analytics/lib/analytic_error_model.dart
@@ -1,22 +1,20 @@
-import 'package:json_annotation/json_annotation.dart';
-
-part 'analytic_error_model.g.dart';
-
-@JsonSerializable()
 class AnalyticErrorModel {
-  // 主键
-  @JsonKey(name: "id")
-  final int? id;
-  final String? data;
+  int? id;
+  String? data;
 
   AnalyticErrorModel({this.id, this.data});
 
-  /// 从 JSON 创建 AnalyticErrorModel 对象
-  factory AnalyticErrorModel.fromJson(Map<String, dynamic> json) =>
-      _$AnalyticErrorModelFromJson(json);
+  AnalyticErrorModel.fromJson(Map<String, dynamic> json) {
+    data = json['data'];
+  }
 
-  /// 将 AnalyticErrorModel 对象转为 JSON
-  Map<String, dynamic> toJson() => _$AnalyticErrorModelToJson(this);
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['id'] = id;
+    data['data'] = data;
+
+    return data;
+  }
 
   /// 数据库表字段
   static Map<String, String> dbColumns() {
